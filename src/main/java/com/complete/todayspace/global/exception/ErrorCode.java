2b868add4445673bb80063ca56607c5827a1c0b9
yj <belle521834@gmail.com>
2024-07-23 19:11:59 +0900

    ♻️ refactor : 띄어쓰기 변경

--- src/main/java/com/complete/todayspace/global/exception/ErrorCode.java
+++ src/main/java/com/complete/todayspace/global/exception/ErrorCode.java
@@ -19,7 +19,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(400, "해당 유저를 찾을 수 없습니다."),
     // Products
     PRODUCT_NOT_FOUND(404,"해당 상품을 찾을 수 없습니다."),
-    NOT_OWNER_PRODUCT(403,"작성자만 변경할 수 있습니다."),
+    NOT_OWNER_PRODUCT(403, "작성자만 변경할 수 있습니다."),
 
     // Posts
 
