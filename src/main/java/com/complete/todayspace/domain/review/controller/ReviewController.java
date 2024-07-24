8af0a31ab9acea5bfdbb414801ca4c58778dd7a1
yj <belle521834@gmail.com>
2024-07-24 20:14:59 +0900

    🎉 add  : ReviewController, ReviewService, ReviewRequestDto 추가

---
+++ src/main/java/com/complete/todayspace/domain/review/controller/ReviewController.java
@@ -0,0 +1,12 @@
+package com.complete.todayspace.domain.review.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class ReviewController {
+
+}
---
+++ src/main/java/com/complete/todayspace/domain/review/service/ReviewService.java
@@ -0,0 +1,10 @@
+package com.complete.todayspace.domain.review.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+}
