da1b48722bef808df61803070d129efa9094f148
yj <belle521834@gmail.com>
2024-07-24 13:43:50 +0900

    ✨ update : 전체 조회 기능 구현

--- src/main/java/com/complete/todayspace/domain/product/repository/ProductRepository.java
+++ src/main/java/com/complete/todayspace/domain/product/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.complete.todayspace.domain.product.repository;
 
+import com.complete.todayspace.domain.product.entity.Address;
 import com.complete.todayspace.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByIdAndUserId(Long productId, Long userId);
+
+    Page<Product> findAllByAddress(Pageable pageable, Address address);
+
+    Page<Product> findProductsByTitleLike(Pageable pageable, String search);
 }
--- src/main/java/com/complete/todayspace/domain/product/service/ProductService.java
+++ src/main/java/com/complete/todayspace/domain/product/service/ProductService.java
@@ -2,12 +2,17 @@ package com.complete.todayspace.domain.product.service;
 
 import com.complete.todayspace.domain.product.dto.CreateProductRequestDto;
 import com.complete.todayspace.domain.product.dto.EditProductRequestDto;
+import com.complete.todayspace.domain.product.dto.ProductDetailResponseDto;
 import com.complete.todayspace.domain.product.dto.ProductResponseDto;
+import com.complete.todayspace.domain.product.entity.Address;
 import com.complete.todayspace.domain.product.entity.Product;
 import com.complete.todayspace.domain.product.repository.ProductRepository;
 import com.complete.todayspace.domain.user.entity.User;
 import com.complete.todayspace.global.exception.CustomException;
 import com.complete.todayspace.global.exception.ErrorCode;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +26,7 @@ public class ProductService {
 
     @Transactional
     public void createProduct(User user, CreateProductRequestDto requestDto) {
+
         Product saveProduct = new Product(requestDto.getTitle(), requestDto.getPrice(),
             requestDto.getContent(),
             requestDto.getAddress(), requestDto.getState(), user);
@@ -30,6 +36,7 @@ public class ProductService {
 
     @Transactional
     public void editProduct(Long id, Long productsId, EditProductRequestDto requestDto) {
+
         Product product = findByProduct(productsId);
         if (!isProductOwner(productsId, id)) {
             throw new CustomException(ErrorCode.NOT_OWNER_PRODUCT);
@@ -40,6 +47,7 @@ public class ProductService {
 
     @Transactional
     public void deleteProduct(Long id, Long productsId) {
+
         Product product = findByProduct(productsId);
         if (!isProductOwner(productsId, id)) {
             throw new CustomException(ErrorCode.NOT_OWNER_PRODUCT);
@@ -48,11 +56,45 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public ProductResponseDto getProduct(Long productsId) {
+    public ProductDetailResponseDto getProduct(Long productsId) {
+
         Product product = findByProduct(productsId);
-        return new ProductResponseDto(product.getId(), product.getUser().getUsername(),
+        return new ProductDetailResponseDto(product.getId(), product.getUser().getUsername(),
             product.getPrice(), product.getTitle(), product.getContent(), product.getAddress(),
             product.getState(), product.getUpdatedAt());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProductSearch(Pageable pageable, String search) {
+
+        Page<Product> Page = productRepository.findProductsByTitleLike(pageable, search);
+        return Page.map(product -> new ProductResponseDto(
+            product.getId(),
+            product.getPrice(),
+            product.getTitle()
+        ));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProductAddress(Pageable pageable, Address address) {
+
+        Page<Product> Page = productRepository.findAllByAddress(pageable, address);
+        return Page.map(product -> new ProductResponseDto(
+            product.getId(),
+            product.getPrice(),
+            product.getTitle()
+        ));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProductPage(Pageable pageable) {
+
+        Page<Product> Page = productRepository.findAll(pageable);
+        return Page.map(product -> new ProductResponseDto(
+            product.getId(),
+            product.getPrice(),
+            product.getTitle()
+        ));
     }
 
     public Product findByProduct(Long productId) {
@@ -64,5 +106,6 @@ public class ProductService {
     private boolean isProductOwner(Long productId, Long userId) {
         return productRepository.existsByIdAndUserId(productId, userId);
     }
+
 }
 
