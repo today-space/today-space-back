b36920a9177e94e4efc3932248f3629e169ac600
rohmac <rohtable@gmail.com>
2024-07-24 20:42:45 +0900

    feat : 게시글 전체 조회
    - 페이지네이션 적용, 5개 최신순

--- src/main/java/com/complete/todayspace/domain/post/repository/PostRepository.java
+++ src/main/java/com/complete/todayspace/domain/post/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.complete.todayspace.domain.post.repository;
 
 import com.complete.todayspace.domain.post.entitiy.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findAllByOrderByUpdatedAtDesc(Pageable pageable);
 }
