b1092c06649c4ff9388cd5c1849950558302b9d8
yj <belle521834@gmail.com>
2024-07-24 20:59:44 +0900

    ✨ update : 후기 추가 기능

--- src/main/java/com/complete/todayspace/domain/review/dto/ReviewRequstDto.java
+++
@@ -1,8 +0,0 @@
-package com.complete.todayspace.domain.review.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ReviewRequstDto {
-
-}
--- src/main/java/com/complete/todayspace/domain/review/service/ReviewService.java
+++ src/main/java/com/complete/todayspace/domain/review/service/ReviewService.java
@@ -1,5 +1,13 @@
 package com.complete.todayspace.domain.review.service;
 
+import com.complete.todayspace.domain.product.entity.Product;
+import com.complete.todayspace.domain.product.service.ProductService;
+import com.complete.todayspace.domain.review.dto.ReviewRequestDto;
+import com.complete.todayspace.domain.review.entity.Review;
+import com.complete.todayspace.domain.review.repository.ReviewRepository;
+import com.complete.todayspace.domain.user.entity.User;
+import com.complete.todayspace.global.exception.CustomException;
+import com.complete.todayspace.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +15,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReviewService {
 
+    private final ReviewRepository reviewRepository;
+    private final ProductService productService;
+
+    public void createReview(User user, Long productsId, ReviewRequestDto requestDto) {
+
+        if (reviewRepository.findByUserIdAndProductId(user.getId(), productsId).isPresent()) {
+            throw new CustomException(ErrorCode.DUPLICATE_REVIEW);
+        }
+        Product product = productService.findByProduct(productsId);
+        Review saveReview = new Review(requestDto.getContent(), product, user);
+
+        reviewRepository.save(saveReview);
+    }
 }
