54f1af5faa10979915ffb62c242fdef5f47db841
rohmac <rohtable@gmail.com>
2024-07-26 11:28:44 +0900

    refactor : 예외처리 부분 수정

--- src/main/java/com/complete/todayspace/domain/comment/service/CommentService.java
+++ src/main/java/com/complete/todayspace/domain/comment/service/CommentService.java
@@ -4,13 +4,13 @@ import com.complete.todayspace.domain.comment.dto.CreateCommentRequestDto;
 import com.complete.todayspace.domain.comment.dto.CommentResponseDto;
 import com.complete.todayspace.domain.comment.entity.Comment;
 import com.complete.todayspace.domain.comment.repository.CommentRepository;
+import com.complete.todayspace.domain.post.entitiy.Post;
 import com.complete.todayspace.domain.post.repository.PostRepository;
 import com.complete.todayspace.domain.user.entity.User;
 import com.complete.todayspace.global.exception.CustomException;
 import com.complete.todayspace.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +19,16 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
 
-    @Transactional
     public CommentResponseDto addComment(User user, Long postId, CreateCommentRequestDto requestDto) {
-        var post = postRepository.findById(postId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-
+        Post post = findPostById(postId);
         Comment comment = new Comment(requestDto.getContent(), post, user);
         commentRepository.save(comment);
 
         return new CommentResponseDto(comment.getId(), comment.getContent(), comment.getPost().getId(), comment.getUser().getId(), comment.getCreatedAt());
     }
 
+    private Post findPostById(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+    }
 }
