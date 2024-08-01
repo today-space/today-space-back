9063ca4b5ea486ceedc2600698f9b4544baee136
youngju <ysy561356@naver.com>
2024-08-01 15:56:17 +0900

    ✨ update : 게시글 단건 조회 추가

--- src/main/java/com/complete/todayspace/domain/post/controller/PostController.java
+++ src/main/java/com/complete/todayspace/domain/post/controller/PostController.java
@@ -91,6 +91,16 @@ public class PostController {
         return new ResponseEntity<>(post, HttpStatus.OK);
     }
 
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<DataResponseDto<PostResponseDto>> getPost(
+        @Min(1) @PathVariable Long postId
+    ) {
+        PostResponseDto responseDto = postService.getPost(postId);
+
+        DataResponseDto<PostResponseDto> response = new DataResponseDto<>(SuccessCode.POSTS_GET, responseDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 
     @PutMapping("/posts/{postId}")
     public ResponseEntity<StatusResponseDto> editPost(
