9d1ecf759921114989da6aa4c460c8ea792c1773
youngju <ysy561356@naver.com>
2024-08-02 16:26:40 +0900

    ✨ update : 게시글 단건 조회 작성자 정보 추가

--- src/main/java/com/complete/todayspace/domain/post/repository/PostRepositoryQueryImpl.java
+++ src/main/java/com/complete/todayspace/domain/post/repository/PostRepositoryQueryImpl.java
@@ -5,6 +5,7 @@ import static com.complete.todayspace.domain.hashtag.entity.QHashtagList.hashtag
 import static com.complete.todayspace.domain.like.entity.QLike.like;
 import static com.complete.todayspace.domain.post.entitiy.QImagePost.imagePost;
 import static com.complete.todayspace.domain.post.entitiy.QPost.post;
+import static com.complete.todayspace.domain.user.entity.QUser.user;
 
 import com.complete.todayspace.domain.common.S3Provider;
 import com.complete.todayspace.domain.hashtag.dto.HashtagDto;
@@ -37,22 +38,32 @@ public class PostRepositoryQueryImpl implements PostRepositoryQuery{
                 Projections.list(Projections.constructor(HashtagDto.class,
                     hashtagList.hashtagName
                 )),
-                like.count()
+                like.count(),
+                user.profileImage,
+                user.username
             ))
             .from(post)
             .leftJoin(imagePost).on(imagePost.post.id.eq(post.id))
             .leftJoin(hashtag).on(hashtag.post.id.eq(post.id))
             .leftJoin(hashtagList).on(hashtag.hashtagList.id.eq(hashtagList.id)) // Make sure to join hashtagList
             .leftJoin(like).on(like.post.id.eq(post.id))
+            .leftJoin(user).on(user.id.eq(post.user.id))
             .where(post.id.eq(postId))
-            .groupBy(post.id, imagePost.id, hashtagList.hashtagName) // GROUP BY 절 추가
+            .groupBy(post.id, imagePost.id, hashtagList.hashtagName, user.id) // GROUP BY 절 추가
             .fetchOne();
 
         // PostImageDto의 S3 URL 변환
-        if (postQuery != null && postQuery.getImages() != null) {
-            List<PostImageDto> updatedImages = postQuery.getImages().stream()
-                .peek(imageDto -> imageDto.update(s3Provider.getS3Url(imageDto.getImagePath()))).collect(Collectors.toList());
-            postQuery.update(updatedImages);
+        if (postQuery != null) {
+            if (postQuery.getImages() != null) {
+                List<PostImageDto> updatedImages = postQuery.getImages().stream()
+                    .peek(imageDto -> imageDto.update(s3Provider.getS3Url(imageDto.getImagePath())))
+                    .collect(Collectors.toList());
+                postQuery.update(updatedImages);
+            }
+            if (postQuery.getProfileImage() != null) {
+                 String updatedImages = s3Provider.getS3Url(postQuery.getProfileImage());
+                postQuery.updateProfile(updatedImages);
+            }
         }
 
         return postQuery;
