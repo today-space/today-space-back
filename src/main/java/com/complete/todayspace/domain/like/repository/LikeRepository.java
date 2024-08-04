70cefcce00b5603226ce554c35bb64c5bf0f443a
rohmac <rohtable@gmail.com>
2024-08-04 21:34:26 +0900

    feat : 커뮤니티 게시글 수정/삭제

--- src/main/java/com/complete/todayspace/domain/like/repository/LikeRepository.java
+++ src/main/java/com/complete/todayspace/domain/like/repository/LikeRepository.java
@@ -4,6 +4,7 @@ import com.complete.todayspace.domain.like.entity.Like;
 import com.complete.todayspace.domain.post.dto.PostMainResponseDto;
 import com.complete.todayspace.domain.post.entitiy.Post;
 import com.complete.todayspace.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -13,4 +14,6 @@ public interface LikeRepository extends JpaRepository<Like, Long>,
 
     Optional<Like> findByUserIdAndPostId(Long id, Long postId);
     long countByPostId(Long postId);
+
+    List<Like> findByPostId(Long postId);
 }
