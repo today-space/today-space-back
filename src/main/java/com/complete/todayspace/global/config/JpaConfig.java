0c10ef0da93787587956560b9d10fec159f098aa
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-08-19 15:44:56 +0900

    refactor: JpaConfig 리팩토링
    JpaConfig 필요없는 코드 제거

--- src/main/java/com/complete/todayspace/global/config/JpaConfig.java
+++ src/main/java/com/complete/todayspace/global/config/JpaConfig.java
@@ -11,22 +11,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class JpaConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-
-    @PersistenceContext
-    private EntityManager entityManager2;
-
-
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
         return new JPAQueryFactory(entityManager);
-    }
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory2() {
-        return new JPAQueryFactory(entityManager2);
     }
 
 }
